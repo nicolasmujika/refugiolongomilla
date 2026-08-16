ca7fea1ea89710543db3b30c5867b6e7ad182a09
nicolasmujika <nicolasmujicacortes12@gmail.com>
2026-08-16 12:00:31 -0400

    Corrige collectstatic para que corra en el arranque, no en el build

--- Dockerfile
+++ Dockerfile
@@ -15,8 +15,7 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-RUN python manage.py collectstatic --noinput --skip-checks || true
 
 EXPOSE 8000
 
-CMD ["gunicorn", "config.wsgi:application", "--bind", "0.0.0.0:8000"]
+CMD python manage.py collectstatic --noinput --skip-checks && gunicorn config.wsgi:application --bind 0.0.0.0:8000
--- config/settings.py
+++ config/settings.py
@@ -12,8 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = config('SECRET_KEY', default='clave-temporal-solo-para-build-no-usar-en-produccion')# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
